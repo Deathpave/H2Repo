e5f976b02d0f5aeedcee4bb339d1b9dd91a73a28
Martin Madsen <martin_riehn_madsen@hotmail.com>
2021-01-07 07:43:12 +0100

    started project

---
+++ AsymEncrypt/AsymEncrypt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AsymEncrypt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
